49c2447f8b6e4502f6da8ca2737aeeff31e32632
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-06 10:58:55 +0200

    Create maven project

---
+++ src/main/java/de/allianz/RERERER/RerererApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.RERERER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RerererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RerererApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/RERERER/RerererApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.RERERER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RerererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
